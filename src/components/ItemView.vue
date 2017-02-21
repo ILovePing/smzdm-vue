0d9ac00a600b9aba264de4c2693007d9b3647406
chenzhengrong <654015195@qq.com>
2017-02-21 22:39:19 +0800

    add router

--- src/components/ItemView.vue
+++ src/components/ItemView.vue
@@ -1,24 +1,39 @@
 <template>
-<div>
-  <img :src="src"> {{pageTitle}}-{{pageSubTitle}} asdfas
+<div class="contend-padded">
+  <img :src="src" style="width:100%;height:12rem;"> {{pageTitle}}-{{pageSubTitle}}
 </div>
 </template>
 <script>
+
 export default {
   beforeRouteEnter(to, from, next) {
     next(vm => {
-      // 通过 `vm` 访问组件实例
+      // 通过 `vm` 访问组件实例,一开始没有生成实例的时候不能用this.$http
       vm.fetchPageData(vm);
     })
   },
   methods: {
     fetchPageData(vm) {
-      console.log(vm.$route.params.pageId);
+      vm.$http.get('http://localhost:3000/getPage?pageId='+vm.$route.params.pageId)
+      .then((res) => {
+        console.log(res)
+        if(res.status == 200 && res.data){
+          vm.pageTitle = res.data.title;
+          vm.pageSubTitle = res.data.subtitle;
+          vm.src = res.data.imgurl;
+        }else{
+          console.log(res.statusText)
+        }
+      }).catch((err) => {
+          console.log(err)
+        })
     }
   },
   data() {
     return {
-
+      pageTitle:'',
+      pageSubTitle:'',
+      src:''
     }
   },
   computed: {
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // src/main.js
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
+// import VueResource from 'vue-resource'
 import store from './store'
 import App from './App'
 import router from './router'
-
+import axios from 'axios';
+Vue.prototype.$http = axios;
 // import 'bootstrap/dist/css/bootstrap.css'
 
-Vue.use(VueResource)
+// Vue.use(VueResource)
 
 router.beforeEach((to,from,next)=>{
     console.log(to.path+'   '+from.path)
